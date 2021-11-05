bd46f0c4a8491ef15bfd1da265d579198734498a
JT Linco <jtcofie@gmail.com>
2021-11-05 15:45:33 +0000

    chore(install): install packages for application

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Welcome</title>
+      </Head>
+
+      <main>
+        Welcome to application
+      </main>
+    </div>
+  )
+}
+
+export default Home
